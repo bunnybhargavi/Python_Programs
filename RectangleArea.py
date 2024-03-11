dee19e02a2d02fb3c7d32e62643afa8b8d902a4c
Bhargavi Amillineni <147232054+bunnybhargavi@users.noreply.github.com>
2024-03-11 21:31:48 +0530

    Add files via upload

---
+++ RectangleArea.py
@@ -0,0 +1,6 @@
+len= float(input("Enter length of a Rectangle:"))
+bre=float(input("Enter breadth of a Rectangle:"))
+Area=len*bre
+Perimeter=2*(len+bre)
+print("Area of Rectangle:",Area)
+print("Perimeter of Rectangle:",Perimeter)
